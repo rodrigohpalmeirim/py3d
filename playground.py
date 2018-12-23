3104d1fb99867bedf45532e82fed7d267182bb38
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2018-12-23 13:26:18 +0000

    added grid; added pause; added fps control

--- playground.py
+++ playground.py
@@ -1,9 +1,34 @@
 from engine import *
-import random
+import random, sys, time
 from math import pi
+from graphics import Text
 
-w = GraphWin("Test", 500, 500, autoflush=False)
+w = GraphWin("Test", 800, 600, autoflush=False)
 w.setBackground("black")
+
+def clear(window, update=True):
+    for item in window.items[:]:
+        item.undraw()
+
+quit = False
+paused = False
+
+def pause():
+    global paused
+    if not paused:
+        paused = True
+    else:
+        paused = False
+
+def keypress(event):
+    global quit
+    if event.char == "q":
+        quit = True
+    elif event.char == "p":
+        pause()
+
+w.focus_set()
+w.bind('<Key>', keypress)
 
 cube = Solid({
     1: [0, 0, 0],
@@ -56,22 +81,74 @@ T = Solid({
     [13, 14], [14, 15], [15, 16], [16, 13]
 ])
 
-thing = cube
-thing.move(200, 200, 200)
+grid_spacing = 100
+grid_size = 3*w.getWidth()
 
-thing.draw(w)
+grid_points = {}
+grid_connections = []
+for i, pos in enumerate(range(0, grid_size+1, grid_spacing)):
+    index = i*4
+    grid_points[index] = [w.getWidth()//2-grid_size//2+pos, w.getHeight(), 0]
+    grid_points[index+1] = [w.getWidth()//2-grid_size//2+pos, w.getHeight(), grid_size]
+    grid_points[index+2] = [w.getWidth()//2-grid_size//2, w.getHeight(), pos]
+    grid_points[index+3] = [w.getWidth()//2+grid_size//2, w.getHeight(), pos]
+    grid_connections += [[index, index+1], [index+2, index+3]]
+
+grid = Solid(grid_points, grid_connections)
+
+grid.move(0, 0, 1)
+
+grid.draw(w, "gray")
+
+thing = cube
+thing.move(w.getWidth()//2-50, w.getHeight()//2-50, 150)
 
 rand1 = random.random()*2*pi
 rand2 = random.random()*2*pi
 rand3 = random.random()*2*pi
 
-for i in range(10000):
-    clear(w)
-    thing.rotate(thing.center(), "x", radians(sin(i/100*(rand1/4)+rand2)*2))
-    thing.rotate(thing.center(), "z", radians(sin(i/100*(rand2/4)+rand3)*2))
-    thing.rotate(thing.center(), "y", radians(sin(i/100*(rand3/4)+rand1)*2))
-    thing.rotate((250, 250, 50), "z", radians(0))
-    thing.draw(w, "white")
-    sleep(0.01)
+start_time = time.time()
+last_time = start_time
+last_tick_durations = []
+
+max_fps = 60
+
+i = 0
+while True:
+
+    tick_duration = time.time()-last_time
+    
+    if tick_duration < 1/max_fps:
+        time.sleep(1/max_fps - tick_duration)
+        tick_duration = time.time()-last_time
+
+    last_time = time.time()
+    last_tick_durations.insert(0, tick_duration)
+    try: last_tick_durations.pop(19)
+    except: pass
+
+    if w.isClosed() or quit:
+        sys.exit()
+
+    if paused:
+        time.sleep(0.05)
+        w.update()
+        continue
+
+    clear(w, update=False)
+    thing.rotate(thing.center(), "x", tick_duration*sin(i*0.01*(rand1/4)+rand2)*2)
+    thing.rotate(thing.center(), "z", tick_duration*sin(i*0.01*(rand2/4)+rand3)*2)
+    thing.rotate(thing.center(), "y", tick_duration*sin(i*0.01*(rand3/4)+rand1)*2)
+    
+    grid.draw(w, update=False)
+    thing.draw(w, "white", update=False)
+    
+    fps = Text(Point(20, 15), round(1/(sum(last_tick_durations)/len(last_tick_durations)), 1))
+    fps.setTextColor("white")
+    fps.draw(w)
+    w.update()
+    fps.undraw()
+
+    i += 1
 
 w.getMouse()
